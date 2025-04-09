f72721de0a154655a2dda336c51c626a127228a7
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-09 09:41:03 +0200

    ADD snack1 bonus

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,10 @@ if (typeof data === "string") {
   console.log(data * 2);
 } else if (typeof data === "boolean") {
   data = true ? console.log("Yes") : console.log("No");
+} else if (typeof data === null) {
+  console.log("Il dato è vuoto");
+} else if (Array.isArray(data)) {
+  console.log(data.length);
+} else if (data instanceof Promise) {
+  data.then((res) => console.log(res));
 } else console.log("Tipo non supportato");
