adf2bcf8a4ae2fbab2a2a1708c802db0d4979242
giovanni-arduini <gionnibigudd@gmail.com>
2025-04-09 10:06:35 +0200

    ADD snack3 bonus

--- src/main.ts
+++ src/main.ts
@@ -37,3 +37,10 @@ type ProjectManager = Dipendente & {
   budgetGestito?: number;
   stakeholderPrincipali: string[];
 };
+
+type Team = {
+  nome: string;
+  progettoAttuale: string | null;
+  budget: number;
+  membri: [ProjectManager, Developer, ...Developer[]];
+};
